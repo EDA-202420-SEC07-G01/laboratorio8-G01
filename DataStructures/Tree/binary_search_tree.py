58f00890ad6ca426649c9f22bb3717837c19c5d2
JesusMOB <jm.otero@uniandes.edu.co>
2024-10-22 15:37:38 -0500

    put bst

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -12,28 +12,42 @@ def new_map():
 
 def put(my_bst, key, value):
     
-    if my_bst['root'] is None:
-        my_bst['root'] = bn.new_node(key,value)
-        my_bst['size'] += 1
+    if my_bst is None:
+        my_bst = new_map()
         return my_bst
     
-    root = my_bst['root']
-        
-    if root['key'] == key:
-        root['value'] = value
-        return my_bst
-            
-    if key < root['key']:
-        if root['left'] is None:
-            root['left'] = bn.new_node(key, value)
-            my_bst['size'] += 1
-        else:
-            put(root['left'], key, value)
+    if 'root' in my_bst:
+        root = my_bst['root']
     else:
-        if root['right'] is None:
-            root['right'] = bn.new_node(key, value)
-            my_bst['size'] += 1
-        else:
-            put(root['right'], key, value)
-            
+        root = my_bst
+    
+    if root is None:
+        root = my_bst['root'] = bn.new_node(key, value)
+        root['size'] = 1
+        return my_bst
+    else:
+        if root['key'] == key:
+            root['value'] = value
+            return my_bst
+        
+        elif key < root['key']:
+            if root['left'] is None:
+                root['left'] = bn.new_node(key, value)
+                root['size'] += 1
+            else:
+                put(root['left'], key, value)
+
+        elif key > root['key']:
+            if root['right'] is None:
+                root['right'] = bn.new_node(key, value)
+                root['size'] += 1
+            else:
+                put(root['right'], key, value)
+                
+    root['size'] = 1
+    if root['left'] is not None:
+        root['size'] += root['left']['size']
+    if root['right'] is not None:
+        root['size'] += root['right']['size']
+                
     return my_bst
